6351a13750affabdf38b5c5ee04abcdbe9ba7557
sarupahari <sarupahari@gmail.com>
2021-09-13 21:20:45 -0500

    Validate user input

--- TicTacToe/HumanPlayer.cs
+++ TicTacToe/HumanPlayer.cs
@@ -12,24 +12,35 @@ namespace TicTacToe
         {
             while (true)
             {
+                int playerInput;
                 Console.WriteLine($"{PlayerValue} enter your move");
-                var playerInput = int.Parse(Console.ReadLine());               
 
-                if (playerInput >= 1 && playerInput <= 9)
+                // validate if user input is a number
+                bool isNumberInput  = int.TryParse(Console.ReadLine(), out playerInput);
+
+                if (isNumberInput)
                 {
-                    var formattedPlayerInput = playerInput - 1;
-                    if (squares[formattedPlayerInput] == EnumSquareValue.Default)
+
+                    if (playerInput >= 1 && playerInput <= 9)
                     {
-                        return formattedPlayerInput;
+                        var formattedPlayerInput = playerInput - 1;
+                        if (squares[formattedPlayerInput] == EnumSquareValue.Default)
+                        {
+                            return formattedPlayerInput;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Spot {playerInput} is taken. Please enter a new move");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine($"Spot {playerInput} is taken. Please enter a new move");
+                        Console.WriteLine("Please enter a valid move");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Please enter a valid move");
+                    Console.WriteLine("Please enter a number between 1 to 9 inclusive.");
                 }
             }
         }
